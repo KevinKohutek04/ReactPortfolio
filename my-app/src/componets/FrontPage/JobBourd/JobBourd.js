7cd1451547e6be6df94f5c99a679a7db68dd98ba
KevinKohutek04 <kohutekkevin7@gmail.com>
2024-06-09 22:43:55 -0500

    fixes

--- my-app/src/componets/FrontPage/JobBourd/JobBourd.js
+++ my-app/src/componets/FrontPage/JobBourd/JobBourd.js
@@ -6,14 +6,14 @@ const JobBourd = () => {
     return (
       <div className="JobBourd" id="experiences">
         <Job link="https://homedepot.com" types="Java SpringBoot MySQL JWT" title="Entry Software Engineer" 
-        subtext="&nbsp;Built and maintained components across Home Depot servers, allowing associates to easily and securely access the server through protected API endpoints. 
-        Worked closely with other developers to roll out new software and major changes, resulting in a significantly improved system for tracking items and tools, 
-        which had a positive impact on store operations and shrinkage.
-          " date="2022 - present" />
-        <Job link="google.com" types="rwq" title="title" subtext="description" date="2020 - 2022" />
+        subtext={<span>* Built and maintained components across Home Depot servers, allowing associates to easily and securely access the server through protected API endpoints.
+          <br /><br />* Worked closely with other developers to roll out new software and major changes, resulting in a significantly improved system for tracking items and tools, which had a positive impact on store operations and shrinkage.</span>} 
+        date="2022 - present" 
+      />
+
+        <Job link="https://homedepot.com" types="SpringBoot Java" title="Home Depot Intership Program" subtext={<span>I spent my time learning about the modern production life cycle,
+           hand ow the code is deployed across all of the stores servers, and the different roles within a development team work and what there tasks are this was a incredbile experince to have.</span>} date="2022 - 2022" />
         
-        <Job link="google.com" types="java css html" title="engineer" subtext="description" date="2022 - present" />
-        <Job link="google.com" types="rwq" title="title" subtext="description" date="2020 - 2022" />
 
         <a href="/Resume_edited.pdf" target="_blank" rel="noopener noreferrer" className="TextCapp">
         View Resume
--- my-app/src/componets/FrontPage/ProjectBourd/ProjectBourd.js
+++ my-app/src/componets/FrontPage/ProjectBourd/ProjectBourd.js
@@ -5,11 +5,10 @@ import Project from './Project.js';
 const ProjectBourd = () => {
     return (
        <div className="ProjectBourd" id="projects">
-            <Project image='project1' title='JWT Quick Start' subtext='A simple jwt spring boot guide that explains how to setup and start a jwt protected project' topic='SpringBoot JWT Java' link='https://github.com/KevinKohutek04/JWT_STARTER/blob/main/README.md'/>
-            <Project image='project1' title='JWT Quick Start' subtext='A simple jwt spring boot guide that explains how to setup and start a jwt protected project' topic='SpringBoot JWT Java' link='https://github.com/KevinKohutek04/JWT_STARTER/blob/main/README.md'/>
-            <Project image='project1' title='JWT Quick Start' subtext='A simple jwt spring boot guide that explains how to setup and start a jwt protected project' topic='SpringBoot JWT Java' link='https://github.com/KevinKohutek04/JWT_STARTER/blob/main/README.md'/>
-            <Project image='project1' title='JWT Quick Start' subtext='A simple jwt spring boot guide that explains how to setup and start a jwt protected project' topic='SpringBoot JWT Java' link='https://github.com/KevinKohutek04/JWT_STARTER/blob/main/README.md'/>
-            <Project image='project1' title='JWT Quick Start' subtext='A simple jwt spring boot guide that explains how to setup and start a jwt protected project' topic='SpringBoot JWT Java' link='https://github.com/KevinKohutek04/JWT_STARTER/blob/main/README.md'/>
+            <Project image='project1' title='JWT Quick Start' 
+            subtext='A simple jwt spring boot guide that explains how to setup and start a jwt protected project' 
+            topic='SpringBoot JWT Java' 
+            link='https://github.com/KevinKohutek04/JWT_STARTER/blob/main/README.md'/>
             
         
        </div>
