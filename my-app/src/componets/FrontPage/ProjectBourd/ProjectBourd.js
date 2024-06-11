4a48ca0f2771ec0c916eb5f1d2fdb2b2a24cfd10
KevinKohutek04 <kohutekkevin7@gmail.com>
2024-06-11 18:13:02 -0500

    added project

--- my-app/src/componets/FrontPage/ProjectBourd/ProjectBourd.js
+++ my-app/src/componets/FrontPage/ProjectBourd/ProjectBourd.js
@@ -5,12 +5,18 @@ import Project from './Project.js';
 const ProjectBourd = () => {
     return (
        <div className="ProjectBourd" id="projects">
+
             <Project image='project1' title='JWT Quick Start' 
             subtext='A simple jwt spring boot guide that explains how to setup and start a jwt protected project' 
             topic='SpringBoot JWT Java' 
             link='https://github.com/KevinKohutek04/JWT_STARTER/blob/main/README.md'/>
             
-        
+            <Project image='KevinKohutek' title='React Portfolio' 
+            subtext='My source code for this project is public, so anyone can see how these components were built. Check it out here.' 
+            topic='React JavaScript Router CSS HTML' 
+            link='https://github.com/KevinKohutek04/ReactPortfolio'/>
+
+
        </div>
     );
 };
